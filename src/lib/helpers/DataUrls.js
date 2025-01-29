599ba1a70fd40fa77eb254a2bf9f8f215740020b
ValeriiViachalo <valeraviachalo@gmail.com>
2025-01-29 12:39:55 +0200

    Remove react-lazyload dependency and update related components for improved performance

--- src/lib/helpers/DataUrls.js
+++ src/lib/helpers/DataUrls.js
@@ -1,5 +1,5 @@
-const URL_BASE = `http://localhost:3000/data/`
-// const URL_BASE = `${process.env.BASE_URL}/data/`
+// const URL_BASE = `http://localhost:3000/data/`
+const URL_BASE = `${process.env.NEXT_PUBLIC_BASE_URL}/data/`
 
 export const URL_HEADER = URL_BASE + "/header.json"
 export const URL_CONTACT = URL_BASE + "/contact.json"
